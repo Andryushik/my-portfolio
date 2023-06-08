c5925844e7b01b42db57dba916a391debbe16309
Andrei <andryushik@gmail.com>
2023-06-08 18:03:39 +0200

    favicon

--- app/components/Devicons.js
+++ app/components/Devicons.js
@@ -2,133 +2,141 @@ import Image from "next/image";
 
 export default function Devicons() {
   return (
-    <div className="p-5 flex justify-between gap-2">
+    <div className="p-5 flex flex-wrap justify-evenly gap-3">
       <div>
         <Image
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg"
           alt="HTML"
-          height={50}
-          width={50}
+          height={48}
+          width={48}
         />
       </div>
       <div>
         <Image
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"
           alt="CSS"
-          height={50}
-          width={50}
+          height={48}
+          width={48}
         />
       </div>
       <div>
         <Image
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg"
           alt="Tailwind-css"
-          height={50}
-          width={50}
+          height={48}
+          width={48}
         />
       </div>
       <div>
         <Image
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg"
           alt="React.js"
-          height={50}
-          width={50}
+          height={48}
+          width={48}
         />
       </div>
       <div className="bg-white rounded-full p-1">
         <Image
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original-wordmark.svg"
           alt="Next.js"
-          height={46}
-          width={46}
+          height={40}
+          width={40}
         />
       </div>
       <div>
         <Image
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
           alt="JavaScript"
-          height={50}
-          width={50}
+          height={48}
+          width={48}
         />
       </div>
       <div className="bg-white rounded-full p-1">
         <Image
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg"
           alt="Node.js"
-          height={46}
-          width={46}
+          height={40}
+          width={40}
         />
       </div>
       <div>
         <Image
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg"
           alt="Sass"
-          height={50}
-          width={50}
+          height={48}
+          width={48}
         />
       </div>
       <div>
         <Image
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg"
           alt="Typescript"
-          height={50}
-          width={50}
+          height={48}
+          width={48}
         />
       </div>
       <div>
         <Image
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/materialui/materialui-original.svg"
           alt="MaterialUI"
-          height={50}
-          width={50}
+          height={48}
+          width={48}
         />
       </div>
       <div>
         <Image
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg"
           alt="Bootstrap"
-          height={50}
-          width={50}
+          height={48}
+          width={48}
         />
       </div>
       <div className="bg-white rounded-full p-1">
         <Image
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg"
           alt="Express"
-          height={46}
-          width={46}
+          height={40}
+          width={40}
         />
       </div>
       <div>
         <Image
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain-wordmark.svg"
           alt="Firebase"
-          height={50}
-          width={50}
+          height={48}
+          width={48}
         />
       </div>
       <div>
         <Image
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-plain-wordmark.svg"
           alt="MongoDB"
-          height={50}
-          width={50}
+          height={48}
+          width={48}
         />
       </div>
       <div>
         <Image
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original-wordmark.svg"
           alt="MySQL"
-          height={50}
-          width={50}
+          height={48}
+          width={48}
         />
       </div>
       <div>
         <Image
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-plain-wordmark.svg"
           alt="GIT"
-          height={50}
-          width={50}
+          height={48}
+          width={48}
+        />
+      </div>
+      <div className="bg-white rounded-full p-1">
+        <Image
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg"
+          alt="Figma"
+          height={40}
+          width={40}
         />
       </div>
     </div>
