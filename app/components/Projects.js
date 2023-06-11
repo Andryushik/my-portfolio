562603d519a7e21ca2aa5fc3a22b4ba2c1134de6
Andrei <andryushik@gmail.com>
2023-06-11 20:23:10 +0200

    projects carts ready

--- app/components/Projects.js
+++ app/components/Projects.js
@@ -18,33 +18,72 @@ export default function Projects() {
       </div>
 
       <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
-        <div className="basis-1/3 flex-1">
+        <div className="group relative overflow-hidden shadow-xl md:shadow-2xl dark:shadow-slate-700 rounded-xl basis-1/3 flex-1">
+          <div className="group-hover:bg-gradient-to-t from-black w-full h-full absolute z-40 transition-all duration-300"></div>
           <Image
+            className="group-hover:scale-125 transition-all duration-500"
             src={web1}
-            alt="temp"
-            className="w-full h-full rounded-lg object-cover"
+            alt="" // TODO: add alts here
           />
+          <div className="absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50">
+            <span className="text-xl text-slate-500">
+              MERN Stack Social Web App
+            </span>
+          </div>
+          <div className="absolute -bottom-full left-12 group-hover:bottom-12 transition-all duration-700 z-50">
+            <span className="text-2xl text-text-head">MY DIARY</span>
+          </div>
         </div>
-        <div className="basis-1/3 flex-1">
+
+        <div className="group relative overflow-hidden shadow-xl md:shadow-2xl dark:shadow-slate-700 rounded-xl basis-1/3 flex-1">
+          <div className="group-hover:bg-gradient-to-t from-black w-full h-full absolute z-40 transition-all duration-300"></div>
           <Image
+            className="group-hover:scale-125 transition-all duration-500"
             src={web4}
-            alt="temp"
-            className="w-full h-full rounded-lg object-cover"
+            alt=""
           />
+          <div className="absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50">
+            <span className="text-xl text-slate-500">
+              MERN Stack Social Web App
+            </span>
+          </div>
+          <div className="absolute -bottom-full left-12 group-hover:bottom-12 transition-all duration-700 z-50">
+            <span className="text-2xl text-text-head">MY DIARY</span>
+          </div>
         </div>
-        <div className="basis-1/3 flex-1">
+
+        <div className="group relative overflow-hidden shadow-xl md:shadow-2xl dark:shadow-slate-700 rounded-xl basis-1/3 flex-1">
+          <div className="group-hover:bg-gradient-to-t from-black w-full h-full absolute z-40 transition-all duration-300"></div>
           <Image
+            className="group-hover:scale-125 transition-all duration-500"
             src={web5}
-            alt="temp"
-            className="w-full h-full rounded-lg object-cover"
+            alt=""
           />
+          <div className="absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50">
+            <span className="text-xl text-slate-500">
+              MERN Stack Social Web App
+            </span>
+          </div>
+          <div className="absolute -bottom-full left-12 group-hover:bottom-12 transition-all duration-700 z-50">
+            <span className="text-2xl text-text-head">MY DIARY</span>
+          </div>
         </div>
-        <div className="basis-1/3 flex-1">
+
+        <div className="group relative overflow-hidden shadow-xl md:shadow-2xl dark:shadow-slate-700 rounded-xl basis-1/3 flex-1">
+          <div className="group-hover:bg-gradient-to-t from-black w-full h-full absolute z-40 transition-all duration-300"></div>
           <Image
+            className="group-hover:scale-125 transition-all duration-500"
             src={web6}
-            alt="temp"
-            className="w-full h-full rounded-lg object-cover"
+            alt=""
           />
+          <div className="absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50">
+            <span className="text-xl text-slate-500">
+              MERN Stack Social Web App
+            </span>
+          </div>
+          <div className="absolute -bottom-full left-12 group-hover:bottom-12 transition-all duration-700 z-50">
+            <span className="text-2xl text-text-head">MY DIARY</span>
+          </div>
         </div>
       </div>
 
