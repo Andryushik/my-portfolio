1ff3d502b0f4f23b2f1eeb18f282191b06d9e07d
Andrei <andryushik@gmail.com>
2023-06-15 20:04:01 +0200

    bg back

--- app/components/Intro.js
+++ app/components/Intro.js
@@ -3,7 +3,7 @@ import SocialLinks from "./SocialLinks";
 export default function Intro() {
   return (
     <section id="home">
-      <div className="bg-avatar h-screen bg-fixed bg-left-bottom bg-contain">
+      <div className="bg-avatar h-screen bg-fixed bg-left-bottom bg-contain bg-no-repeat">
         <div className="px-7 md:px-16 lg:px-28 xl:px-40 flex flex-col md:flex-row md:justify-end items-center h-screen bg-white/20 dark:bg-black/20 bg-gradient-to-l from-black dark:from-white">
           <div className="text-center md:text-right md:w-3/5 py-20 md:py-10">
             <h1 className="text-2xl sm:3xl md:text-4xl lg:text-5xl xl:text-5xl 2xl:text-6xl py-5 text-transparent bg-clip-text bg-gradient-to-br from-cyan-500 to-teal-500 font-medium drop-shadow-lg">
