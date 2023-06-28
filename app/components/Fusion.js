36b59ad179ae1219bf214751177b57ce462bc7b3
Andrei <andryushik@gmail.com>
2023-06-28 16:19:41 +0200

    clean-up

--- app/components/Fusion.js
+++
@@ -1,9 +0,0 @@
-export default function Fusion() {
-  return (
-    <div className="relative w-80 md:w-[500px] h-[400px] md:h-[600px] max-w-lg">
-      <div className="absolute top-24 -left-8 w-64 h-64 md:w-96 md:h-96 rounded-full bg-purple-300 mix-blend-multiply animate-blob filter blur-3xl opacity-90" />
-      <div className="absolute -right-10 w-64 h-64 md:w-96 md:h-96 rounded-full bg-yellow-300 mix-blend-multiply animate-blob filter blur-3xl opacity-90 animation-delay-4000" />
-      <div className="absolute bottom-0 -right-10 w-64 h-64 md:w-96 md:h-96 rounded-full bg-pink-300 mix-blend-multiply animate-blob filter blur-3xl opacity-90 animation-delay-2000" />
-    </div>
-  );
-}
--- tailwind.config.js
+++ tailwind.config.js
@@ -19,34 +19,6 @@ module.exports = {
           },
         ],
       },
-      // dropShadow: {
-      //   glow: [
-      //     "0 0px 1px rgba(255, 255, 255, 0.4)",
-      //     "0 0px 4px rgba(255, 255, 255, 0.2)",
-      //   ],
-      // },
-      // animation: {
-      //   blob: "blob 9s infinite",
-      // },
-      // keyframes: {
-      //   blob: {
-      //     "0%": {
-      //       transform: "translate(0px, 0px) scale(1)",
-      //     },
-      //     "25%": {
-      //       transform: "translate(-20px, -60px) scale(1.1)",
-      //     },
-      //     "50%": {
-      //       transform: "translate(30px, -10px) scale(0.9)",
-      //     },
-      //     "75%": {
-      //       transform: "translate(20px, 60px) scale(1.1)",
-      //     },
-      //     "100%": {
-      //       transform: "translate(0px, 0px) scale(1)",
-      //     },
-      //   },
-      // },
     },
   },
   plugins: [],
