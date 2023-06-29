4d6695499a56264ccd5a6abe6de36a9f6e6355c3
Andrei <andryushik@gmail.com>
2023-06-29 16:05:11 +0200

    contact form UI done

---
+++ app/components/ContactForm.js
@@ -0,0 +1,66 @@
+import React from "react";
+
+export default function ContactForm() {
+  return (
+    <div className="py-8 px-4 mx-auto max-w-screen-md">
+      <h4 className="mb-6 text-2xl tracking-tight font-bold text-center text-text-head">
+        Message Me
+      </h4>
+      <p className="mb-8 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
+        Wanna hire me? Got a question? Want to send feedback? Let me know.
+      </p>
+      <form onSubmit="" className="space-y-8">
+        <div>
+          <label
+            for="email"
+            className="block mb-2 font-normal text-gray-900 dark:text-gray-300"
+          >
+            Your email
+          </label>
+          <input
+            type="email"
+            id="email"
+            className="block w-full p-2.5 bg-gray-50 border border-gray-300 text-gray-900 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white focus:outline-text-head"
+            placeholder="name@gmail.com"
+            required
+          />
+        </div>
+        <div>
+          <label
+            for="subject"
+            className="block mb-2 font-medium text-gray-900 dark:text-gray-300"
+          >
+            Subject
+          </label>
+          <input
+            type="text"
+            id="subject"
+            className="block w-full p-2.5 bg-gray-50 border border-gray-300 text-gray-900 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white focus:outline-text-head"
+            placeholder="Let me know how I can help you"
+            required
+          />
+        </div>
+        <div className="sm:col-span-2">
+          <label
+            for="message"
+            className="block mb-2 font-medium text-gray-900 dark:text-gray-300"
+          >
+            Your message
+          </label>
+          <textarea
+            id="message"
+            rows="6"
+            className="block w-full p-2.5 bg-gray-50 border border-gray-300 text-gray-900 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white focus:outline-text-head"
+            placeholder="Leave a comment..."
+          ></textarea>
+        </div>
+        <button
+          type="submit"
+          className="py-3 px-5 font-medium text-center text-white rounded-lg bg-text-head sm:w-fit hover:bg-text-head/70"
+        >
+          Send message
+        </button>
+      </form>
+    </div>
+  );
+}
--- app/sections/Contact.js
+++ app/sections/Contact.js
@@ -1,26 +1,26 @@
 import { BsGlobeEuropeAfrica } from "react-icons/bs";
 import SocialLinks from "../components/SocialLinks";
-// import ContactForm from "./ContactForm";
+import ContactForm from "../components/ContactForm";
 
 export default function Contact() {
   return (
     <section id="contact">
       <div className="py-14 md:py-24 px-7 md:px-16 lg:px-28 xl:px-40">
         <h3 className="text-2xl md:text-3xl text-text-head py-1 drop-shadow-md">
-          Contact
+          Contacts
         </h3>
 
-        {/* TODO: */}
-        {/* <ContactForm /> */}
+        <div className="flex justify-center">
+          <SocialLinks />
+        </div>
 
         <div className="flex justify-center gap-4 items-end md:justify-start py-8 text-gray-800 dark:text-gray-200">
           <BsGlobeEuropeAfrica className="text-2xl text-slate-400" />
           <span>Located in the Netherlands</span>
         </div>
 
-        <div className="flex justify-center">
-          <SocialLinks />
-        </div>
+        <ContactForm />
+
         <p className="text-sm md:text-base pt-12 leading-8 text-gray-800 dark:text-gray-200">
           Thank you for visiting my portfolio, and I look forward to the
           possibility of working together!
