5dac9eceb1b838ce83f731211b7d9ff0e4b9c580
Andrei <andryushik@gmail.com>
2024-07-14 17:25:17 +0200

    refactor: Update Hero section styles and effects

--- app/sections/Hero.js
+++ app/sections/Hero.js
@@ -19,9 +19,9 @@ export default function Hero() {
 
   return (
     <section id="hero" ref={heroRef}>
-      <div className="relative h-screen w-full flex md:items-center justify-center md:justify-end px-7 md:px-16 lg:px-28 xl:px-40 overflow-hidden bg-[#CDCED2] dark:bg-gray-800">
+      <div className="relative h-screen w-full flex md:items-center justify-center md:justify-end px-7 md:px-16 lg:px-28 xl:px-40 overflow-hidden">
         <motion.div
-          className="absolute inset-0 z-0"
+          className="absolute inset-0 z-0 shadow-2xl dark:shadow-gray-700"
           style={{ y, opacity, scale }}
         >
           <Image
@@ -41,6 +41,7 @@ export default function Hero() {
             duration: 1,
             delay: 0.5,
           }}
+          style={{ y, opacity, scale }}
         >
           <h1 className="text-3xl md:text-4xl lg:text-6xl 2xl:text-7xl py-3 md:py-5 text-transparent bg-clip-text bg-gradient-to-br from-cyan-600 to-teal-600 font-medium">
             Hi, <br />
